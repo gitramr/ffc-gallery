c08c943662e5cf0e9a2034b3db16f5b6f1ce5ef5
gitkailas <kailasv.1997@gmail.com>
2025-11-22 23:34:30 +0530

    Auto-update manifest and assets

--- manifest.js
+++ manifest.js
@@ -1,18 +1,5 @@
 const manifest = [
-  "image00002.jpg",
-  "image00003.jpg",
-  "image00004.jpg",
   "image00005.jpg",
-  "image00006.jpg",
-  "image00007.jpg",
-  "image00008.jpg",
-  "image00009.jpg",
-  "image00010.jpg",
-  "image00011.jpg",
-  "image00012.jpg",
-  "image00013.jpg",
-  "image00014.jpg",
-  "image00015.jpg",
   "image00016.jpg",
   "image00017.jpg",
 ];
